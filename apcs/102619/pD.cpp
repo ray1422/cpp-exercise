27da8633dd40e23767af3e8edb788db99ebc5030
Ray Chung <ray20020809@gmail.com>
2019-10-26 19:41:14 +0800

    add sth

--- apcs/102619/pD.cpp
+++ apcs/102619/pD.cpp
@@ -1,15 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 int _min(int a, int b, int c, int d){return min(a, min(b, min(c, d)));}
-bool m[4][4] = {
-{0, 1, 0, 1},
-{1, 0, 1, 0},
-{0, 1, 0, 1},
-{1, 0, 1, 0}
-
-};
+bool m[30][30];
 bool hasSet[30][30][30][30];
-int history[30][30][30][30];
+short history[30][30][30][30];
 int getCost(bool is_row, int a, int b, int c) {
     int cost = 0;
     if(is_row){
@@ -26,8 +20,8 @@ int getCost(bool is_row, int a, int b, int c) {
     return cost;
 }
 int dp(int h, int w, int h_min, int w_min) {
-    if(hasSet[h][w][h_min][w_min]) return history[h][w][h_min][w_min];
     if(h-h_min <= 1 || w - w_min <= 1) return 0;
+    if(hasSet[h][w][h_min][w_min]) return history[h][w][h_min][w_min];
     int costL = getCost(true,  h_min, h, w_min) + dp(h, w, h_min, w_min + 1);
     int costR = getCost(true,  h_min, h, w-1) + dp(h, w - 1, h_min, w_min);
     int costT = getCost(false, w_min, w, h_min) + dp(h, w, h_min + 1, w_min);
@@ -38,5 +32,6 @@ int dp(int h, int w, int h_min, int w_min) {
     return cost;
 }
 int main() {
-    cout << dp(4, 4, 0, 0);
+    for(int i = 0 ; i <25*25; i++) m[i/25][i%25]=i%2;
+    cout << dp(25, 25, 0, 0);
 }
